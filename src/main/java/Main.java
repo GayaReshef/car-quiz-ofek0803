a0bd4a244409d49845087e16ff620f05ab544795
GayaReshef <gaya.reshef@gmail.com>
2024-10-06 13:01:32 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,43 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+public class CarInfo {
+public boolean illegal (int maxSpeed) {
+		if (this.speed > maxSpeed || this.privateCar == true){
+			return true;
+		}
+	return false;
+	}
 }
+
+
+public class CameraInfo {
+public boolean allGood (CarInfo [] cars) {
+	for (int i = 0; i < cars.length; i++) {
+		if (cars[i].illegal (maxSpeed) == true) {
+			return false;
+		}
+	}
+	return true;
+}
+}
+
+
+public static int legalcities (CameraInfo [] cameras) {
+	int count = 0;
+	boolean oneCity = true;
+	for (int i = 0; i < 100; i++) {
+		for (int j = 0; j< cameras.length; j++) {
+			if (cameras[j].getCity() == i) {
+				if (cameras[j].allGood() == true){
+					oneCity = true;
+				}
+				else {
+					oneCity = false;
+				}
+			}
+			if (oneCity == true) {
+				count++;
+			}
+		}
+	}
+return count;	
+}
+
